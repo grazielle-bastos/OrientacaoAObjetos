801d698340f7067f4af8b1fac1a8c0a56a10aeb2
grazielle-bastos <grazielle1101@gmail.com>
2025-08-22 22:04:33 -0300

    feat: implementa ControleRemoto com encapsulamento e validações

---
+++ src/E/Encapsulamento/Main5.java
@@ -0,0 +1,14 @@
+package E.Encapsulamento;
+
+public class Main5 {
+    public static void main(String[] args) {
+
+        ControleRemoto c = new ControleRemoto();
+
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+
+    }
+}
