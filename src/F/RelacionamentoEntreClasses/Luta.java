ac4bbed2ce62d27103e810fa4b0676b9e77eaa88
grazielle-bastos <grazielle1101@gmail.com>
2025-08-23 23:23:32 -0300

    feat: implementa sistema de lutadores com relacionamento entre classes

---
+++ src/F/RelacionamentoEntreClasses/Luta.java
@@ -0,0 +1,18 @@
+package F.RelacionamentoEntreClasses;
+
+public abstract class Luta {
+
+    private String desafiado;
+    private String desafiante;
+    private int rouds;
+    private boolean aprovada;
+
+    void marcarLuta() {
+
+    }
+
+    void lutar() {
+
+    }
+
+}
