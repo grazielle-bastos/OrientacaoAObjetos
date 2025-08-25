3c9cbbd4c1a70bfc20d6e4443650a81032e64384
grazielle-bastos <grazielle1101@gmail.com>
2025-08-24 23:33:55 -0300

    feat: implementa sistema completo de lutas com agregação

--- src/F/RelacionamentoEntreClasses/Luta.java
+++ src/F/RelacionamentoEntreClasses/Luta.java
@@ -2,10 +2,42 @@ package F.RelacionamentoEntreClasses;
 
 public abstract class Luta {
 
-    private String desafiado;
-    private String desafiante;
+    private Lutador desafiado;
+    private Lutador desafiante;
     private int rouds;
     private boolean aprovada;
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRouds() {
+        return rouds;
+    }
+
+    public void setRouds(int rouds) {
+        this.rouds = rouds;
+    }
+
+    public boolean getAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
 
     void marcarLuta() {
 
---
+++ src/G/RelacionamentoDeAgregacao/Luta.java
@@ -0,0 +1,103 @@
+package G.RelacionamentoDeAgregacao;
+
+import java.sql.SQLOutput;
+import java.util.Random;
+
+public class Luta {
+
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rouds;
+    private boolean aprovada;
+
+    public Luta() {
+
+    }
+
+    public Luta(Lutador desafiado, Lutador desafiante, int rouds, boolean aprovada) {
+        this.desafiado = desafiado;
+        this.desafiante = desafiante;
+        this.rouds = rouds;
+        this.aprovada = aprovada;
+    }
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRouds() {
+        return rouds;
+    }
+
+    public void setRouds(int rouds) {
+        this.rouds = rouds;
+    }
+
+    public boolean getAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+
+    void marcarLuta(Lutador l1, Lutador l2) {
+        if (l1.getCategoria().equals(l2.getCategoria())
+                && l1 != l2) {
+            this.aprovada = true;
+            this.desafiado = l1;
+            this.desafiante = l2;
+        } else {
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+        }
+    }
+
+    void lutar() {
+        if (this.aprovada) {
+            System.out.println("### DESAFIADO ###");
+            this.desafiado.apresentar();
+            System.out.println("### DESAFIANTE ###");
+            this.desafiante.apresentar();
+
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3);
+            System.out.println("### RESULTADO DA LUTA ###");
+            switch(vencedor) {
+                case 0: //empate
+                    System.out.println("### EMPATOU ###");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    break;
+                case 1: //vitória do desafiado
+                    System.out.println("Vitória do " + this.desafiado.getNome());
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                    break;
+                case 2: //vitória do desafiante
+                    System.out.println("Vitória do " + this.desafiante.getNome());
+                    this.desafiante.ganharLuta();
+                    this.desafiado.perderLuta();
+                    break;
+            }
+            System.out.println("===============================");
+
+        } else {
+            System.out.println("Luta não pode acontecer!");
+        }
+    }
+
+}
